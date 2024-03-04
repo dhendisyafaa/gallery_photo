2f2b878c03c31ea1c740dd4dd1b5610422924ddc
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-03-04 17:59:02 +0700

    feat: add new url cors

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://radsnaps.vercel.app"],
   })
 );
 // app.use(bodyParser.json());
